ef66d0ba61a69b8d46e680157e56731ca7546ae5
GidCalds <100079699+GidCalds@users.noreply.github.com>
2022-02-21 16:29:38 +0800

    Update index.php

--- index.php
+++ index.php
@@ -9,9 +9,6 @@
  <ul>
  <li><a href="index.php">Home</a></li>
  <li><a href="https://github.com/GidCalds/learningtasks.git"> LEARNING TASK 2 </a></li>
-  <li><a href="Omelette.html">Omelette</a></li>
-  <li><a href="Beef in Beer.html">Beef in Beer</a></li>
-  <li><a href="Pad Thai Chicken.html">Pad Thai Chicken</a></li>
  </ul>
  
  </body>
