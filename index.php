64144b71300f220ecd3f67df57d5d55690eff370
GidCalds <100079699+GidCalds@users.noreply.github.com>
2022-02-21 15:30:22 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,17 @@
 <!Doctype html>
 <html>
 <head>
- <title> My homepage </title>
+ <title> My Recipe Homepage </title>
  </head>
  
  <body>
- <center><h1> Welcome to my webpage </h1></center>
+ <center><h1> Welcome to my Recipe Webpage </h1></center>
  <ul>
  <li><a href="index.php">Home</a></li>
  <li><a href="https://github.com/GidCalds/learningtasks.git"> LEARNING TASK 2 </a></li>
+  <li><a href="Omelette.html">Omelette</a></li>
+  <li><a href="Beef in Beer.html">Beef in Beer</a></li>
+  <li><a href="Pad Thai Chicken.html">Pad Thai Chicken</a></li>
  </ul>
  
  </body>
