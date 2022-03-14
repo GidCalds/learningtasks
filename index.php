0a0f437dce84674b3d870147a3b09ad044186cf6
Arakku <100070895+Arakku@users.noreply.github.com>
2022-03-14 22:43:43 +0800

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
  <center><h1> Welcome to my Webpage. Hi! my name if Gideon Calderon</h1></center>
  <ul>
  <li><a href="index.php">Home</a></li>
- <li><a href="https://github.com/GidCalds/learningtasks.git"> LEARNING TASK 2 </a></li>
+ <li><a href="Top 10 Celebrities.html"> LEARNING TASK 2 </a></li>
  </ul>
  
  </body>
